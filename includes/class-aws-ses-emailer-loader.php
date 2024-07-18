8a408fe3419d6e7197bea813d5101f6dd6fd8ef8
calvinrodrigues500 <calvinrodrigues500@gmail.com>
2024-07-19 00:27:30 +0530

    Initial template

---
+++ includes/class-aws-ses-emailer-loader.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * WP AWS SES emailer.
+ *
+ * @package wp-aws-ses-emailer
+ */
+
+namespace Calvin\WpAwsSesEmailer;
+
+defined('ABSPATH') || die;
+
+class AWS_SES_Emailer_Loader {
+
+	public function __construct() {
+	}
+}
---
+++ wp-aws-ses-emailer.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: WP AWS SES Integration
+ * Plugin URI: https://calvinrodrigues.in
+ * Description: AWS SES Integration for WordPress
+ * Version: 1.0.0
+ * Requires PHP 7.4
+ * Author: Calvin Rodrigues
+ * Author URI: https://calvinrodrigues.in
+ */
+
+
+defined('ABSPATH') || die;
+
+require __DIR__ . '/vendor/autoload.php';
