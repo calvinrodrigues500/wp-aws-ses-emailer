baec68f7cced4dfd85b43118571edda7717fe119
calvinrodrigues500 <calvinrodrigues500@gmail.com>
2024-07-29 23:57:23 +0530

    Add select control

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from './Navbar';
 export { default as Content } from './Content';
 export { default as SettingsPage } from './SettingsPage';
 export { default as InputControl } from './InputControl';
+export { default as SelectControl } from  './SelectControl';
