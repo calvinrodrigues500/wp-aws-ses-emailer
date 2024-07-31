64659aa63051190d16b59669a01f935a798b3e6b
calvinrodrigues500 <calvinrodrigues500@gmail.com>
2024-07-31 23:08:57 +0530

    Implemented settings API

--- includes/Endpoints/V1/Settings.php
+++ includes/Endpoints/V1/Settings.php
@@ -38,6 +38,13 @@ class Settings {
 	protected $endpoint = 'settings';
 
 	/**
+	 * Settings option name.
+	 *
+	 * @var string $option_name
+	 */
+	private $option_name = 'wp_aws_ses_emailer_settings';
+
+	/**
 	 * Settings constructor.
 	 */
 	public function __construct() {
@@ -61,17 +68,37 @@ class Settings {
 	 */
 	public function register_routes() {
 
-			register_rest_route(
-				$this->namepsace,
-				$this->endpoint,
+		register_rest_route(
+			$this->namepsace,
+			$this->endpoint,
+			array(
 				array(
-					array(
-						'methods'	=> WP_REST_Server::CREATABLE,
-						'callback'	=> array($this, 'save_settings'),
-						'permission_callback'	=> array($this, 'check_permission')
-					)
+					'methods'	=> WP_REST_Server::CREATABLE,
+					'callback'	=> array($this, 'save_settings'),
+					'permission_callback'	=> array($this, 'check_permission'),
+					'args'		=> array(
+						'awsSesAccessKeyId'	=> array(
+							'required'		=> true,
+							'description'	=> __('AWS Access Key ID associated with the IAM user.', 'wp-aws-ses-emailer'),
+							'type'			=> 'string',
+							'sanitize_callback'		=> array(__CLASS__, 'sanitize_data')
+						),
+						'awsSesSecretAccessKey'	=> array(
+							'required'			=> true,
+							'description'		=> __('AWS Secret Key ID associated with the IAM user.', 'wp-aws-ses-emailer'),
+							'type'				=> 'string',
+							'sanitize_callback'	=> array(__CLASS__, 'sanitize_data')
+						),
+						'awsSesRegion'			=> array(
+							'required'			=> true,
+							'description'		=> __('AWS region', 'wp-aws-ses-emalier'),
+							'type'				=> 'string',
+							'sanitize_callback'	=> array(__CLASS__, 'sanitize_data')
+						)
+					),
 				)
-			);
+			)
+		);
 	}
 
 	/**
@@ -85,6 +112,32 @@ class Settings {
 	 * Save settings.
 	 */
 	public function save_settings($request) {
-		error_log('saving '.print_r($request,1));
+
+		$params = $request->get_params();
+		extract($params);
+
+		if (empty($awsSesAccessKeyId) || empty($awsSesSecretAccessKey)) {
+			wp_send_json_error(
+				array(
+					'message'	=> __('Missing access key or secret access key', 'wp-aws-ses-emailer')
+				),
+				400
+			);
+		}
+
+		update_option($this->option_name, array(
+			'access_key'		=> $awsSesAccessKeyId,
+			'secret_access_key'	=> $awsSesSecretAccessKey,
+			'aws_region'		=> $awsSesRegion
+		));
+	}
+
+	/**
+	 * Sanitze input.
+	 *
+	 * @param mixed.
+	 */
+	public static function sanitize_data($data) {
+		return $data; //@todo Add sanitization.
 	}
 }
